5ab6a29fb5741e4e5a9b6f4dacd89b734fe139db
pablohernanaraujo <pablohernanaraujo@gmail.com>
2021-09-25 10:13:14 -0300

    fix some stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { FC } from 'react';
 export const App: FC = () => (
   <div>
     <header>
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
+      <p>React development optimization</p>
     </header>
   </div>
 );
