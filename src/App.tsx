e0bf3f456b5a5fdfd53278bec49bb7bb6edd0ee8
pablohernanaraujo <pablohernanaraujo@gmail.com>
2021-09-26 15:12:10 -0300

    feature pre commit test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import ThemeProvider from '@theme/context';
 
-export const App: FC = () => (
+export const App = () => (
   <ThemeProvider>
     <header>
       <p>React development optimization</p>
